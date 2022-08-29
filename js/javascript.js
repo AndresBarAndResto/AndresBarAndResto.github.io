7c8174a76d280aaa8412c94d4dff8db41554cb97
unknown <jvbuitizon@gmail.com>
2022-08-29 18:50:25 +0800

    etc

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,24 @@ var nav = document.querySelector('nav');
 
       window.addEventListener('scroll', function () {
         if (window.pageYOffset > 100) {
-          nav.classList.add('bg-black', 'bg-opacity-10', 'text-black');
+          nav.classList.add('bg-black', 'bg-opacity-25', 'text-black');
         } else {
           nav.classList.remove('bg-black', 'bg-opacity-10', 'text-black');
         }
       });
+
+      function myFunction() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more");
+        var btnText = document.getElementById("myBtn");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "Read more";
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = "Read less";
+          moreText.style.display = "inline";
+        }
+      }
