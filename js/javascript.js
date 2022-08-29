da76049f659ea244011dd9a0ad43a738782534a0
unknown <jvbuitizon@gmail.com>
2022-08-29 17:09:21 +0800

    Major Adjustment

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,8 @@ var nav = document.querySelector('nav');
 
       window.addEventListener('scroll', function () {
         if (window.pageYOffset > 100) {
-          nav.classList.add('bg-black', 'bg-opacity-10');
+          nav.classList.add('bg-black', 'bg-opacity-10', 'text-black');
         } else {
-          nav.classList.remove('bg-black', 'bg-opacity-10');
+          nav.classList.remove('bg-black', 'bg-opacity-10', 'text-black');
         }
       });
