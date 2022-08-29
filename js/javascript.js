b5e05d30eca6a0807e7225c94cb49e41ad8c3a59
unknown <jvbuitizon@gmail.com>
2022-08-29 13:44:02 +0800

    link mode

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,8 @@ var nav = document.querySelector('nav');
 
       window.addEventListener('scroll', function () {
         if (window.pageYOffset > 100) {
-          nav.classList.add('bg-primary', 'shadow', 'background-color: rgba(0,0,0, 0.7)');
+          nav.classList.add('bg-black', 'bg-opacity-10');
         } else {
-          nav.classList.remove('bg-primary', 'shadow');
+          nav.classList.remove('bg-black', 'bg-opacity-10');
         }
       });
